8a7dd6401add984e1a9e22ff9d679e0522be8068
吕剑超 <jianchao.lv@ucarinc.com>
2019-08-04 12:02:11 +0800

    新增http配置请求入参是否允许为NUll(不允许时删除对应入参)

--- index.js
+++ index.js
@@ -20,12 +20,23 @@ module.exports = sails => {
         },
 
         routes: {
-          before: {
-            'all /*':(req,res,next) => {
-              res.badRequest = require('./api/responses/badRequest')(req,res);
-              return next();
+            before: {
+                'all /*': (req, res, next) => {
+                    //如配置不允许入参为null,删除入参值为null的属性
+                    if (_.has(sails.config.http, 'params.allowNull')
+                        && !sails.config.http.params.allowNull) {
+                        _.each(req.allParams(), (value, key) => {
+                            if (_.isNull(value)) {
+                                if (_.has(req.params, key)) return delete req.params[key];
+                                if (_.has(req.body, key)) return delete req.body[key];
+                                if (_.has(req.query, key)) return delete req.query[key];
+                            }
+                        });
+                    }
+                    res.badRequest = require('./api/responses/badRequest')(req, res);
+                    return next();
+                }
             }
-          }
         }
     }
 
